1def00a94557a4b2a203b03b39a84ea80016d682
adzs <xkali6x@gmail.com>
2026-01-21 16:36:02 +1100

    Update DevToolboxApp.kt

--- app/src/main/java/com/devtoolbox/DevToolboxApp.kt
+++ app/src/main/java/com/devtoolbox/DevToolboxApp.kt
@@ -1,1 +1,17 @@
+package com.devtoolbox
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import com.devtoolbox.navigation.NavGraph
+import com.devtoolbox.ui.theme.DevToolboxTheme
+
+@Composable
+fun DevToolboxApp() {
+    DevToolboxTheme {
+        Surface(color = MaterialTheme.colorScheme.background) {
+            NavGraph()
+        }
+    }
+}
 
