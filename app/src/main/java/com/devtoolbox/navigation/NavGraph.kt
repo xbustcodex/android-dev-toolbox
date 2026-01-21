413fc8e662d5b0e1d8dad304cb678ef3d42f7d29
adzs <xkali6x@gmail.com>
2026-01-21 16:37:14 +1100

    Update NavGraph.kt

--- app/src/main/java/com/devtoolbox/navigation/NavGraph.kt
+++ app/src/main/java/com/devtoolbox/navigation/NavGraph.kt
@@ -1,1 +1,23 @@
+package com.devtoolbox.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.devtoolbox.ui.home.HomeScreen
+import com.devtoolbox.ui.tools.*
+
+@Composable
+fun NavGraph() {
+    val nav = rememberNavController()
+    NavHost(navController = nav, startDestination = Routes.HOME) {
+        composable(Routes.HOME) { HomeScreen(onOpen = { nav.navigate(it) }) }
+        composable(Routes.LOGCAT) { LogcatScreen() }
+        composable(Routes.INTENT) { IntentLauncherScreen() }
+        composable(Routes.PACKAGES) { PackagesScreen() }
+        composable(Routes.APK_INFO) { ApkInfoScreen() }
+        composable(Routes.DEEPLINK) { DeepLinkScreen() }
+        composable(Routes.CLIPBOARD) { ClipboardScreen() }
+    }
+}
 
