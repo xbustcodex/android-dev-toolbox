ca07ed18a62fadd9fc99329e2f6a285030e2223e
adzs <xkali6x@gmail.com>
2026-01-21 16:35:32 +1100

    Update MainActivity.kt

--- app/src/main/java/com/devtoolbox/MainActivity.kt
+++ app/src/main/java/com/devtoolbox/MainActivity.kt
@@ -1,1 +1,13 @@
+package com.devtoolbox
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { DevToolboxApp() }
+    }
+}
 
