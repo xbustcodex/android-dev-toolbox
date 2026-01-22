17f66ab1fadfb453abdafef193d12e1812c362a4
adzs <xkali6x@gmail.com>
2026-01-22 12:29:47 +1100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
