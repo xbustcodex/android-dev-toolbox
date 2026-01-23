a0204e2ee4f8bd3369571c943479234dbcbe96fe
adzs <xkali6x@gmail.com>
2026-01-23 12:15:44 +1100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.gms.google-services") version "4.4.4" apply false
 }
 
